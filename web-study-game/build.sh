d988a813b4402abe7e858b899b2008f0ba1cedb3
duwlsssss <rladuwls0814@gmail.com>
2025-03-31 23:15:35 +0900

    chore: lint, prettier 설정

--- web-study-game/build.sh
+++ web-study-game/build.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-cd ../
-mkdir output
-cp -R ./WebStudyGame/* ./output
-cp -R ./output ./WebStudyGame/
+mkdir -p output
+cp -R WebStudyGame/* output/
--- web-study-game/main.js
+++
@@ -1,7 +0,0 @@
-import './style.css'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    메인 페이지
-  </div>
-`
