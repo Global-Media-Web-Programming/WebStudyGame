d540461b78adb4e85018020040b96934381a8fe6
duwlsssss <rladuwls0814@gmail.com>
2025-04-01 00:34:52 +0900

    test: 빌드 디렉토리명 변경

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@ echo "📌 현재 디렉토리: $(pwd)"  # 현재 경로 출력
 ls -la  # 현재 디렉토리의 모든 파일 출력
 
 mkdir -p output
-if [ -d "./web-study-game" ]; then
-    echo "📂 web-study-game 디렉토리 존재함"
+if [ -d "./WebStudyGame" ]; then
+    echo "📂 WebStudyGame 디렉토리 존재함"
 else
-    echo "❌ web-study-game 디렉토리가 존재하지 않습니다."
+    echo "❌ WebStudyGame 디렉토리가 존재하지 않습니다."
 fi
 
-if [ "$(ls -A ./web-study-game 2>/dev/null)" ]; then
-    echo "✅ web-study-game 내부에 파일이 있음"
-    cp -R ./web-study-game/* ./output
+if [ "$(ls -A ./WebStudyGame 2>/dev/null)" ]; then
+    echo "✅ WebStudyGame 내부에 파일이 있음"
+    cp -R ./WebStudyGame/* ./output
 else
-    echo "⚠️ 배포 실패: web-study-game 디렉토리가 비어있거나 존재하지 않습니다."
+    echo "⚠️ 배포 실패: WebStudyGame 디렉토리가 비어있거나 존재하지 않습니다."
     exit 1 
 fi
